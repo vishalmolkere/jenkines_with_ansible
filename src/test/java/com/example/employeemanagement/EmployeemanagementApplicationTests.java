4a4849cd241a548d96732ba18538f44a4fd89a6f
vishal molkere <vishalmolkere88@gmail.com>
2026-07-13 14:53:19 +0530

    fix: skip DB autoconfiguration in context loads test

--- src/test/java/com/example/employeemanagement/EmployeemanagementApplicationTests.java
+++ src/test/java/com/example/employeemanagement/EmployeemanagementApplicationTests.java
@@ -1,9 +1,16 @@
 package com.example.employeemanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 class EmployeemanagementApplicationTests {
 
 	@Test
